0e6eb1cfd84bcc48bf61b6b44be748e159e3c8e1
Smig <89040888+smiggiddy@users.noreply.github.com>
2023-02-06 19:21:33 +0000

    added my work in gocourse

--- challenges/types-composite/begin/main.go
+++ challenges/types-composite/begin/main.go
@@ -1,38 +1,60 @@
 // challenges/types-composite/begin/main.go
 package main
 
+import "fmt"
+
 // define an author type with a name
-//
+type author struct {
+	name string
+}
 
 // define a book type with a title and author
-//
+type book struct {
+	title string
+	author
+}
 
 // define a library type to track a list of books
-//
+type library map[string][]book
 
 // define addBook to add a book to the library
-//
+func (l library) addBook(nb book) {
+	l[nb.author.name] = append(l[nb.author.name], nb)
+}
 
 // define a lookupByAuthor function to find books by an author's name
-//
+func (l library) lookupByAuthor(author string) []book {
+	return l[author]
+}
 
 func main() {
 	// create a new library
-	//
+	myLibrary := library{}
 
 	// add 2 books to the library by the same author
-	//
-
+	book1 := book{
+		title:  "Book1",
+		author: author{name: "Some guy"},
+	}
+	book2 := book{
+		title:  "Book2",
+		author: author{name: "Some guy"},
+	}
+	myLibrary.addBook(book1)
+	myLibrary.addBook(book2)
 	// add 1 book to the library by a different author
-	//
-
+	book3 := book{
+		title:  "different",
+		author: author{name: "different guy"},
+	}
+	myLibrary.addBook(book3)
 	// dump the library with spew
-	//
+	fmt.Printf("%#v\n", myLibrary)
 
 	// lookup books by known author in the library
-	//
+	fmt.Println(myLibrary.lookupByAuthor("Some guy"))
 
 	// print out the first book's title and its author's name
-	//
+	fmt.Println(myLibrary.lookupByAuthor("Some guy")[0])
 
 }
--- types/slices/begin/main.go
+++ types/slices/begin/main.go
@@ -1,14 +1,19 @@
 // types/slices/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare a slice string the make function
-	//
+	names := make([]string, 0)
 
 	// append 3 names to the slice
-	//
+	names = append(names, "smig")
+	names = append(names, "testing")
+	names = append(names, "simple")
 
 	// print the slice
+	fmt.Println(names)
 
 	// slice the slice using syntax slice[low:high]
 	// [Jane Mary]
