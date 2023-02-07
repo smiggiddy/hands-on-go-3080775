1ebc3003cdeb054e2ac865515deebb87b6ea0889
Smig <89040888+smiggiddy@users.noreply.github.com>
2023-02-07 18:22:57 +0000

    finished challenge interfaces

--- challenges/interfaces/begin/main.go
+++ challenges/interfaces/begin/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -19,6 +20,61 @@ type letterCounter struct{ identifier string }
 type numberCounter struct{ designation string }
 
 type symbolCounter struct{ label string }
+
+// func (s interface{}) count(i string) int {
+// 	var sum int
+// 	for _, l := range i {
+// 		switch b := s.(type) {
+// 		case letterCounter:
+// 			sum++
+// 		case numberCounter:
+// 			if unicode.IsNumber(l) {
+// 				sum++
+// 			}
+
+// 		case symbolCounter:
+// 			if unicode.IsSymbol(l) {
+// 				sum++
+// 			}
+
+// 		}
+// 	}
+// 	return sum
+// }
+
+func (s symbolCounter) count(input string) (sum int) {
+
+	for _, l := range input {
+		if !unicode.IsLetter(l) && !unicode.IsNumber(l) {
+			sum++
+		}
+	}
+	return
+}
+
+func (n numberCounter) count(input string) (sum int) {
+
+	for _, l := range input {
+		if unicode.IsNumber(l) {
+			sum++
+		}
+	}
+	return
+}
+
+func (let letterCounter) count(input string) (sum int) {
+
+	for _, l := range input {
+		if unicode.IsLetter(l) {
+			sum++
+		}
+	}
+	return
+}
+
+func (s symbolCounter) name() string { return fmt.Sprintf("%v", s.label) }
+func (n numberCounter) name() string { return fmt.Sprintf("%v", n.designation) }
+func (l letterCounter) name() string { return fmt.Sprintf("%v", l.identifier) }
 
 func doAnalysis(data string, counters ...counter) map[string]int {
 	// initialize a map to store the counts
@@ -54,8 +110,12 @@ func main() {
 	data := string(bs)
 	spew.Dump(data)
 
+	s := numberCounter{designation: "numbers"}
+	l := letterCounter{identifier: "letters"}
+	sy := symbolCounter{label: "symbols"}
 	// call doAnalysis and pass in the data and the counters
-
+	fmt.Println(s.designation)
+	analysis := doAnalysis(data, s, l, sy)
 	// dump the map to the console using the spew package
-	// spew.Dump(analysis)
+	spew.Dump(analysis)
 }
--- interfaces/type-switch/begin/main.go
+++ interfaces/type-switch/begin/main.go
@@ -1,11 +1,22 @@
 // interfaces/type-switch/begin/main.go
 package main
 
-// define whatAmI which takes in an argument of any type and returns inforamtion about the underlying value's type
+import "fmt"
 
+// define whatAmI which takes in an argument of any type and returns inforamtion about the underlying value's type
+func whatAmI(val interface{}) string {
+	switch val.(type) {
+	case int:
+		return fmt.Sprintf("%v is an int", val)
+	case string:
+		return fmt.Sprintf("%v is a string", val)
+	default:
+		return fmt.Sprintf("%v is an unspported type", val)
+	}
+}
 func main() {
 	// invoke whatAmI function
-	// fmt.Println(whatAmI(1))
-	// fmt.Println(whatAmI("hello"))
-	// fmt.Println(whatAmI(true))
+	fmt.Println(whatAmI(1))
+	fmt.Println(whatAmI("hello"))
+	fmt.Println(whatAmI(true))
 }
